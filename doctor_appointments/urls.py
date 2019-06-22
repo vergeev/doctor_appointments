689d4805d7ec4131af37ea84af626826de44c3c1
Pavel Vergeev <pvergeev@pm.me>
2019-06-22 17:05:21 +0300

    Make admin the index page

--- doctor_appointments/urls.py
+++ doctor_appointments/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(pattern_name='admin:index', permanent=True)),
     path('admin/', admin.site.urls),
 ]
