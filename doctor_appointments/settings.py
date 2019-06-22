f1f58cab777d21b2af24538e469919eeffa2b0f9
Pavel Vergeev <pvergeev@pm.me>
2019-06-22 17:20:00 +0300

    Make config environment-based

--- doctor_appointments/settings.py
+++ doctor_appointments/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,12 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(lf5!o6-&efs04m)2x&k+fa%h!gmq1*oq)u+knqe^tmhcs&p=1'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+SECRET_KEY = os.getenv('SECRET_KEY', '(lf5!o6-&efs04m)2x&k+fa%h!gmq1')
+DEBUG = os.getenv('DEBUG', 'False')
 ALLOWED_HOSTS = []
 
 
